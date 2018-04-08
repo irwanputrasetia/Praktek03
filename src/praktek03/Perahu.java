b10dc7572f83a5ee9f7ae47f36362e5cb9415a5a
irwanputrasetia <irwanputrastya@gmail.com>
2018-04-08 16:50:30 +0700

    Buat cetakInfo()

--- src/praktek03/Perahu.java
+++ src/praktek03/Perahu.java
@@ -4,4 +4,11 @@ public class Perahu {
    String bahan;
    String warna;
    String harga;
+   
+   void cetakInfo(){
+       System.out.println("Merk \t:  "+bahan+"\n"+
+                           "Warna \t:"+warna+"\n"+
+                           "Harga \t"+harga);
+   
+   }
 }
