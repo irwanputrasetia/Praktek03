e072dda48d3c80f0bcb7459d84a6a88e07747a32
irwanputrasetia <irwanputrastya@gmail.com>
2018-04-08 16:46:52 +0700

    Membuat Attribut

--- src/praktek03/Perahu.java
+++ src/praktek03/Perahu.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package praktek03;
-
-/**
- *
- * @author irwan
- */
-public class Perahu {
     
+public class Perahu {
+   String bahan;
+   String warna;
+   String harga;
 }
